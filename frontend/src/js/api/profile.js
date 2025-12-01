8ef5ae6c8f132888ca7f5645c9b62087a31c33c2
GrahamQuitno <gquitno@ucsd.edu>
2025-11-30 19:28:23 -0800

    Add getProfile()

--- frontend/src/js/api/profile.js
+++ frontend/src/js/api/profile.js
@@ -14,3 +14,15 @@ import { postWrapper } from '/src/js/fetch-wrapper.js';
  */
 
 const BACKEND_URL = 'http://localhost:3001';
+
+/**
+ * @description Get the current user's profile information.
+ * @returns { ProfileInfo } Current user's profile information.
+ */
+export async function getProfile() {
+  let response = await getWrapper(BACKEND_URL + '/api/me/profile');
+  if (!response.ok) {
+    throw new Error(response.error);
+  }
+  return response.data;
+}
