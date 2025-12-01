3063429f49a3b960e20a98cc3117edb016cb4cd4
GrahamQuitno <gquitno@ucsd.edu>
2025-11-30 19:27:51 -0800

    Add ProfileInfo as return type

---
+++ frontend/src/js/api/profile.js
@@ -0,0 +1,16 @@
+import { getWrapper } from '/src/js/fetch-wrapper.js';
+import { postWrapper } from '/src/js/fetch-wrapper.js';
+
+/**
+ * @typedef ProfileInfo
+ * @type { object }
+ * @property { number } id - User's ID number.
+ * @property { string } email - User's email address.
+ * @property { string } first_name - User's first name.
+ * @property { string } last_name - User's last name.
+ * @property { string } pronouns - User's preferred pronouns.
+ * @property { string } global_role -User's role in the class, which determines permissions.
+ * @property { boolean } is_profile_complete - Indicates if the user has filled in their profile with all the appropriate information.
+ */
+
+const BACKEND_URL = 'http://localhost:3001';
