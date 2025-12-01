57791304e806636ea6fb5e55a4bb41b7635b780e
GrahamQuitno <gquitno@ucsd.edu>
2025-11-30 19:28:50 -0800

    Add updateProfile()

--- frontend/src/js/api/profile.js
+++ frontend/src/js/api/profile.js
@@ -26,3 +26,19 @@ export async function getProfile() {
   }
   return response.data;
 }
+
+/**
+ * @description Update the current user's profile information.
+ * @param { object } newValues - An object containing new values to update the current user's profile with:
+ * @param { string } [newValues.first_name] - Current user's first name.
+ * @param { string } [newValues.last_name] - Current user's last name.
+ * @param { string } [newValues.pronouns] - Current user's preferred pronouns.
+ * @returns { ProfileInfo } Current user's profile information.
+ */
+export async function updateProfile(newValues) {
+  let response = await postWrapper(BACKEND_URL + '/api/me/profile', newValues);
+  if (!response.ok) {
+    throw new Error(response.error);
+  }
+  return response.data;
+}
