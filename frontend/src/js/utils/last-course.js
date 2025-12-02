be23205695d44d2a89c179933febaef8691c0c5b
Justin Radatti <jdradatti@gmail.com>
2025-12-02 14:37:43 -0800

    Format

--- frontend/src/js/utils/last-course.js
+++ frontend/src/js/utils/last-course.js
@@ -1,5 +1,5 @@
 // Get the course_id from the path /course/{course_id}/page_name
-const courseId = location.pathname.split("/")[2];
+const courseId = location.pathname.split('/')[2];
 
 // Set cookie to remember last visited course
 document.cookie = `last_course_id=${courseId}; path=/; SameSite=Lax`;
@@ -11,8 +11,7 @@ document.cookie = `last_course_id=${courseId}; path=/; SameSite=Lax`;
  * @returns {string|null} The course ID stored in the `last_course_id` cookie, or `null` if the cookie DNE.
  */
 export function lastCourseId() {
-    const match = document.cookie.match(/(?:^|; )last_course_id=([^;]*)/);
-    const courseId = match ? decodeURIComponent(match[1]) : null;
-    return courseId;
+  const match = document.cookie.match(/(?:^|; )last_course_id=([^;]*)/);
+  const courseId = match ? decodeURIComponent(match[1]) : null;
+  return courseId;
 }
-
