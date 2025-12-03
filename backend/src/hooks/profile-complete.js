603590018af865fd531301bcc2bfb338eb732f7f
MisakizzZZZ <401058751@qq.com>
2025-12-03 05:33:39 -0800

    add more error information about profile-complete hook

--- backend/src/hooks/profile-complete.js
+++ backend/src/hooks/profile-complete.js
@@ -48,6 +48,7 @@ module.exports = fp(async function profileCompleteHook(fastify) {
       );
       return reply.code(403).send({
         error: 'PROFILE_INCOMPLETE',
+        message: 'Make sure that first name, last name are filled out, so that your profile is considered complete.',
       });
     }
   });
