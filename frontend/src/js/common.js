7145305b3c881be1d146c53a7ac7d16b76c84771
Justin Radatti <jdradatti@gmail.com>
2025-12-03 12:52:24 -0800

    Delete last-course.js -- not needed anymore

--- frontend/src/js/common.js
+++ frontend/src/js/common.js
@@ -3,7 +3,6 @@ import '/src/components/index.js';
 
 //Import common JS modules
 import '/src/js/utils/load-theme.js';
-import '/src/js/utils/last-course.js';
 
 //Import ZingGrid
 import '/node_modules/zinggrid/dist/zinggrid.min.js';
