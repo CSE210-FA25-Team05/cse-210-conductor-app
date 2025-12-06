9724c92aad00257b14eba174b18e2bc6f7990b24
GrahamQuitno <gquitno@ucsd.edu>
2025-12-06 13:01:11 -0800

    Update deleteWrapper to have optional body argument

--- frontend/src/js/fetch-wrapper.js
+++ frontend/src/js/fetch-wrapper.js
@@ -117,6 +117,7 @@ export async function getWrapper(url, timeoutDuration = 1000, numRetries = 0) {
 /**
  * @description Makes a DELETE request and processes the response to return a consistent object.
  * @param {string} url - URL to send the HTTP request to.
+ * @param {object} [body] - Body data to be sent with the HTTP request. Don't use JSON.stringify, we do that for you.
  * @param {number} [timeoutDuration] - Number of milliseconds before giving up on the request.
  * @param {number} [numRetries] - Number of times to retry the request when the request does not succeed.
  * @returns { ok: boolean, status: number, data?: json, error?: string} - An object containing:
@@ -127,11 +128,13 @@ export async function getWrapper(url, timeoutDuration = 1000, numRetries = 0) {
  */
 export async function deleteWrapper(
   url,
+  body = {},
   timeoutDuration = 1000,
   numRetries = 0
 ) {
   let options = {
     method: 'DELETE',
+    body: JSON.stringify(body),
   };
   let fetchResponse = await fetchWrapper(
     url,
