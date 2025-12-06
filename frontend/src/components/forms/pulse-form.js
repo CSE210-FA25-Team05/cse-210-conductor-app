0bfd38e8d5c1b77a194c5b34944cae5b283228c5
Justin Radatti <jdradatti@gmail.com>
2025-12-06 09:26:36 -0800

    Update pulse form

--- frontend/src/components/forms/pulse-form.js
+++ frontend/src/components/forms/pulse-form.js
@@ -1,32 +1,50 @@
 import { ConductorForm } from '/src/components/forms/conductor-form.js';
+import { getCachedCourseId } from '/src/js/utils/cache-utils.js';
 
 /**
- * CreateCourseForm Web Component
+ * PulseForm Web Component
  * ------------------------------
- * A form for creating a new course. Extends BaseForm and defines
- * all fields required to create a course.
+ * A form for creating a new Pulse.
  *
- * Fields include:
- *  - course_code
- *  - course_name
- *  - term
- *  - section
- *  - join_code
- *  - start_date
- *  - end_date
+ * First, calls GET /api/courses/course_id/pulses/config
+ * to get the pulse config. Then, dynamically sets the
+ * form elements.
  *
  * On submission, the component performs:
- *  1. POST /api/courses with the collected form data
- *  2. Redirects to /course/:id/dashboard on success
+ *  1. POST /api/courses_id/pulses
  *
  * @extends ConductorForm
  *
  * @example
- * <create-course-form></create-course-form>
+ * <pulse-form></pulse-form>
  */
 class PulseForm extends ConductorForm {
+  constructor() {
+    super();
+    this.config = null;
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // TODO: Build fields dynamically from config
   get fields() {
-    return [{ label: 'Course Code', id: 'course-code', name: 'course_code' }];
+    return [
+      {
+        label: 'How do you feel?',
+        name: 'pulses',
+        type: 'radio-group',
+        options: [
+          { label: 'Happy', value: 'happy', color: 'green' },
+          { label: 'Stressed', value: 'stressed', color: 'red' },
+          { label: 'Neutral', value: 'neutral', color: 'gray' },
+        ],
+      },
+      {
+        label: 'Description',
+        id: 'description',
+        name: 'description',
+        type: 'textarea',
+      },
+    ];
   }
 
   get submitLabel() {
@@ -35,6 +53,20 @@ class PulseForm extends ConductorForm {
 
   async onSubmit(values) {
     console.log('Submit pulse', values);
+
+    const body = {
+      option: values.option,
+      description: values.description,
+    };
+
+    alert('Body:', values, body);
+  }
+
+  getFormValues() {
+    const values = super.getFormValues();
+    if (this.description) values.description = this.description.value;
+    alert(values);
+    return values;
   }
 }
 
