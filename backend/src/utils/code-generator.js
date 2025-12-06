633a1292f40992a29694713783bf3f0317fbdfbf
jayantiii <jlahoti@ucsd.edu>
2025-12-06 15:36:09 -0800

    fix code generator

--- backend/src/utils/code-generator.js
+++ backend/src/utils/code-generator.js
@@ -14,7 +14,7 @@
  */
 function generateCode() {
   const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let generatedCode = '';
   for (let i = 0; i < 6; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
