49a7080288b8290427e49805ca26f7c3d07c1d3e
yangwestyyy21 <33038975+yangwestyyy21@users.noreply.github.com>
2025-12-09 17:48:07 -0800

    did jsdoc+ran linter

--- backend/tracing.js
+++ backend/tracing.js
@@ -1,8 +1,15 @@
 // tracing.js
 const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
-const { ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+const {
+  getNodeAutoInstrumentations,
+} = require('@opentelemetry/auto-instrumentations-node');
+const {
+  OTLPTraceExporter,
+} = require('@opentelemetry/exporter-trace-otlp-http');
+const {
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} = require('@opentelemetry/sdk-trace-base');
 
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter({
