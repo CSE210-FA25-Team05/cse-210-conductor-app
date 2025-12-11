f7277c69b0db21a8badfbb5c4a4bb4fc9ec678be
Peter Lee <chiwoonlee@gmail.com>
2025-12-10 19:19:29 -0800

    student/prof attendance working

--- frontend/src/components/attendance/student-attendance.js
+++ frontend/src/components/attendance/student-attendance.js
@@ -21,67 +21,27 @@ export class StudentAttendance extends HTMLElement {
     }
   }
 
-  // async submitHandler() {
-  //   try {
-  //     const inputCode = this.querySelector('#code-input').value;
-  //     if (inputCode === null || inputCode === '') {
-  //       this.studentMsg.style = 'display:block; color:red;';
-  //       this.studentMsg.textContent = 'Input a valid code.';
-  //       return;
-  //     }
+  async submitHandler() {
+    try {
+      const inputCode = this.querySelector('#code-input').value;
+      if (inputCode === null || inputCode === '') {
+        this.studentMsg.style = 'display:block; color:red;';
+        this.studentMsg.textContent = 'Input a valid code.';
+        return;
+      }
+      this.courseId = parseInt(getCourseId());
 
-  //     this.courseId = parseInt(getCourseId());
-  //     this.lectures = await getLectures(this.courseId);
-
-  //     this.today = new Date().toISOString().split('T')[0];
-
-  //     let todaysLecture = null;
-  //     for (const lec of this.lectures) {
-  //       if (lec.lecture_date === this.today) {
-  //         todaysLecture = lec;
-  //         break;
-  //       }
-  //     }
-
-  //     if (!todaysLecture) {
-  //       this.studentMsg.style = 'display:block; color:red;';
-  //       this.studentMsg.textContent = 'No lecture scheduled for today.';
-  //       return;
-  //     }
-
-  //     if (!todaysLecture.code) {
-  //       this.studentMsg.style = 'display:block; color:red;';
-  //       this.studentMsg.textContent =
-  //         "No attendance code for today's lecture yet.";
-  //       return;
-  //     }
-
-  //     if (todaysLecture.code === inputCode) {
-  //       const now = new Date().toLocaleDateString('en-CA');
-  //       const d = new Date(todaysLecture.code_expires_at).toLocaleDateString(
-  //         'en-CA'
-  //       );
-  //       if (now < d) {
-  //         const output = await createAttendanceSimplified(this.courseId, {
-  //           code: inputCode,
-  //         });
-  //         if (output.user_id == getUserId()) {
-  //           this.studentMsg.style = 'display:block; color:green;';
-  //           this.studentMsg.textContent = 'Code accepted!';
-  //           this.querySelector('#code-input').value = '';
-  //         } else {
-  //           this.studentMsg.style = 'display:block; color:red;';
-  //           this.studentMsg.textContent = 'Wrong attendance code!';
-  //         }
-  //       }
-  //     }
-  //   } catch (e) {
-  //     this.studentMsg.style = 'display:block; color:red;';
-  //     this.studentMsg.textContent =
-  //       'Error logging attendance. Please try again.';
-  //     console.log(`Error logging attendance: ${e}`);
-  //   }
-  // }
+      const output = await createAttendanceSimplified(this.courseId, {
+        code: inputCode,
+      });
+      this.studentMsg.style = 'display:block; color:green;';
+      this.studentMsg.textContent = 'Code accepted!';
+      this.querySelector('#code-input').value = '';
+    } catch (e) {
+      this.studentMsg.style = 'display:block; color:red;';
+      this.studentMsg.textContent = e;
+    }
+  }
 
   async createStudentView() {
     this.studentView = document.createElement('div');
@@ -120,55 +80,6 @@ export class StudentAttendance extends HTMLElement {
     );
     this.studentMsg = this.studentView.querySelector('#student-msg');
     this.submitBtn.addEventListener('click', () => this.submitHandler());
-
-    // // dynamic scripts
-    // // check if code is active
-    // try {
-    //   this.courseId = parseInt(getCourseId());
-    //   this.lectures = await getLectures(this.courseId);
-    //   this.today = new Date().toLocaleDateString('en-CA');
-    //   let todaysLecture = null;
-    //   for (const lec of this.lectures) {
-    //     if (lec.lecture_date === this.today) {
-    //       todaysLecture = lec;
-    //       break;
-    //     }
-    //   }
-
-    //   if (!todaysLecture) {
-    //     return;
-    //   }
-    //   const userAttendance = await getAttendanceStats(this.courseId);
-    //   if (userAttendance.lectures) {
-    //     for (const att of userAttendance.lectures) {
-    //       if (att.lecture_id == todaysLecture.id) {
-    //         this.attendanceForm.style.display = 'none';
-    //         this.studentMsg.style = 'display:block; color:green;';
-    //         this.studentMsg.textContent =
-    //           'You have already gotten credit for attendance today.';
-    //         return;
-    //       }
-    //     }
-    //   }
-    //   if (todaysLecture.code) {
-    //     const now = new Date();
-    //     const d = new Date(todaysLecture.code_expires_at);
-    //     if (now < d) {
-    //       this.studentState.textContent = 'Code active!';
-    //       this.studentTimerRow.style = 'display: block;';
-    //       const expireTime = d.toLocaleTimeString([], {
-    //         hour: '2-digit',
-    //         minute: '2-digit',
-    //       });
-    //       this.studentExpiration.textContent = `Code expires at: ${expireTime}`;
-    //     } else {
-    //       this.studentState.textContent = 'Code expired.';
-    //     }
-    //   }
-    // } catch (e) {
-    //   console.error(e);
-    //   return;
-    // }
   }
 }
 
